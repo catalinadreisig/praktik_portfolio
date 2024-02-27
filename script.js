8e12ffbc9e7251e0199650076bce8da1c5b3dd38
catalinadreisig <catalinadreisig@yahoo.dk>
2024-02-27 11:27:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLink(overlay) {
+  var link = overlay.querySelector(".hidden-link");
+  link.style.display = "block";
+}
+
+function hideLink(overlay) {
+  var link = overlay.querySelector(".hidden-link");
+  link.style.display = "none";
+}
